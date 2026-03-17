5fc7c8a46746867a34c270f3ef1e2343273f1be2
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-03-17 15:54:54 -0600

    Update process_sample.R

--- utils/scripts/dada2/process_sample.R
+++ utils/scripts/dada2/process_sample.R
@@ -7,21 +7,19 @@ cat("Processing sample:", sam, "\n")
 
 # Read in the filtered reads for this sample
 filtF <- snakemake@input[["R1"]]
-filtR <- snakemake@input[["R2"]]
 
 
 # Load the error models (these are assumed to be created already)
 load(snakemake@input[["errR1"]])  # loads object `errF`
-load(snakemake@input[["errR2"]])  # loads object `errR`
 
-mergers <- vector("list", length(sam))
+
 dadaFs <- vector("list", length(sam))
 
-names(mergers) <- sam
+
 names(dadaFs) <- sam
 
 names(filtF) <- sam
-names(filtR) <- sam
+
 
 # Dereplicate and run DADA2 on the forward and reverse reads
 set.seed(100)
@@ -31,10 +29,8 @@ ddF <- dada(derepF, err = errF, multithread = snakemake@threads)
 
 dadaFs[[sam]] <- ddF
 
-derepR <- derepFastq(filtR)
-ddR <- dada(derepR, err = errR, multithread = snakemake@threads)
 
 saveRDS(ddF, snakemake@output[["ddF"]])
-saveRDS(ddR, snakemake@output[["ddR"]])
+
 saveRDS(derepF, snakemake@output[["derepF"]])
-saveRDS(derepR, snakemake@output[["derepR"]])
+
